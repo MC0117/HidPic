b5c63a7a8356f1a783cc6072f1ec0119039260fe
manfredclase01 <manfred.clase@gmail.com>
2018-12-11 23:13:37 +0100

    Added OpenFileDialog, SaveFileDialog

--- StegPic/StegPic/Forms/Form1.Designer.cs
+++ StegPic/StegPic/Forms/Form1.Designer.cs
@@ -28,29 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.initialPictureBox = new System.Windows.Forms.PictureBox();
             this.decryptRadioBtn = new System.Windows.Forms.RadioButton();
             this.encryptRadioBtn = new System.Windows.Forms.RadioButton();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.hiddenInformationTextBox = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.initialPicturePathLabel = new System.Windows.Forms.Label();
+            this.picturePathTextBox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.addKeyButton = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.methodLabel = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.ofdOpenButton = new System.Windows.Forms.Button();
+            this.fileNameLabel = new System.Windows.Forms.Label();
+            this.saveFileLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.saveFilePathTextBox = new System.Windows.Forms.TextBox();
+            this.sdfOpenButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.initialPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // initialPictureBox
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(15, 97);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(549, 318);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.initialPictureBox.Location = new System.Drawing.Point(15, 97);
+            this.initialPictureBox.Name = "initialPictureBox";
+            this.initialPictureBox.Size = new System.Drawing.Size(549, 318);
+            this.initialPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.initialPictureBox.TabIndex = 0;
+            this.initialPictureBox.TabStop = false;
             // 
             // decryptRadioBtn
             // 
@@ -59,7 +66,6 @@
             this.decryptRadioBtn.Name = "decryptRadioBtn";
             this.decryptRadioBtn.Size = new System.Drawing.Size(58, 17);
             this.decryptRadioBtn.TabIndex = 1;
-            this.decryptRadioBtn.TabStop = true;
             this.decryptRadioBtn.Text = "Extract";
             this.decryptRadioBtn.UseVisualStyleBackColor = true;
             this.decryptRadioBtn.Click += new System.EventHandler(this.decryptRadioBtn_Click);
@@ -67,6 +73,7 @@
             // encryptRadioBtn
             // 
             this.encryptRadioBtn.AutoSize = true;
+            this.encryptRadioBtn.Checked = true;
             this.encryptRadioBtn.Location = new System.Drawing.Point(12, 25);
             this.encryptRadioBtn.Name = "encryptRadioBtn";
             this.encryptRadioBtn.Size = new System.Drawing.Size(51, 17);
@@ -76,43 +83,43 @@
             this.encryptRadioBtn.UseVisualStyleBackColor = true;
             this.encryptRadioBtn.Click += new System.EventHandler(this.encryptRadioBtn_Click);
             // 
-            // richTextBox1
+            // hiddenInformationTextBox
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 550);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(552, 173);
-            this.richTextBox1.TabIndex = 3;
-            this.richTextBox1.Text = "";
+            this.hiddenInformationTextBox.Location = new System.Drawing.Point(15, 538);
+            this.hiddenInformationTextBox.Name = "hiddenInformationTextBox";
+            this.hiddenInformationTextBox.Size = new System.Drawing.Size(549, 173);
+            this.hiddenInformationTextBox.TabIndex = 3;
+            this.hiddenInformationTextBox.Text = "";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 523);
+            this.label1.Location = new System.Drawing.Point(12, 511);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(116, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "Unencrypted message:";
             // 
-            // label4
+            // initialPicturePathLabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 74);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(68, 13);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "Picture URL:";
+            this.initialPicturePathLabel.AutoSize = true;
+            this.initialPicturePathLabel.Location = new System.Drawing.Point(12, 62);
+            this.initialPicturePathLabel.Name = "initialPicturePathLabel";
+            this.initialPicturePathLabel.Size = new System.Drawing.Size(68, 13);
+            this.initialPicturePathLabel.TabIndex = 10;
+            this.initialPicturePathLabel.Text = "Picture URL:";
             // 
-            // textBox2
+            // picturePathTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(79, 71);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(244, 20);
-            this.textBox2.TabIndex = 11;
+            this.picturePathTextBox.Location = new System.Drawing.Point(79, 59);
+            this.picturePathTextBox.Name = "picturePathTextBox";
+            this.picturePathTextBox.Size = new System.Drawing.Size(287, 20);
+            this.picturePathTextBox.TabIndex = 11;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(45, 459);
+            this.label2.Location = new System.Drawing.Point(45, 447);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(28, 13);
             this.label2.TabIndex = 5;
@@ -120,14 +127,14 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(79, 456);
+            this.textBox1.Location = new System.Drawing.Point(79, 444);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(152, 20);
             this.textBox1.TabIndex = 6;
             // 
             // addKeyButton
             // 
-            this.addKeyButton.Location = new System.Drawing.Point(79, 482);
+            this.addKeyButton.Location = new System.Drawing.Point(79, 470);
             this.addKeyButton.Name = "addKeyButton";
             this.addKeyButton.Size = new System.Drawing.Size(62, 26);
             this.addKeyButton.TabIndex = 7;
@@ -137,7 +144,8 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 435);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 423);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(552, 13);
             this.label3.TabIndex = 9;
@@ -147,6 +155,7 @@
             // methodLabel
             // 
             this.methodLabel.AutoSize = true;
+            this.methodLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.methodLabel.Location = new System.Drawing.Point(9, 9);
             this.methodLabel.Name = "methodLabel";
             this.methodLabel.Size = new System.Drawing.Size(357, 13);
@@ -155,56 +164,123 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 729);
+            this.button1.Location = new System.Drawing.Point(5, 787);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 13;
             this.button1.Text = "Perform";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // ofdOpenButton
+            // 
+            this.ofdOpenButton.Location = new System.Drawing.Point(372, 57);
+            this.ofdOpenButton.Name = "ofdOpenButton";
+            this.ofdOpenButton.Size = new System.Drawing.Size(75, 23);
+            this.ofdOpenButton.TabIndex = 14;
+            this.ofdOpenButton.Text = "Choose...";
+            this.ofdOpenButton.UseVisualStyleBackColor = true;
+            this.ofdOpenButton.Click += new System.EventHandler(this.ofdOpenButton_Click);
+            // 
+            // fileNameLabel
+            // 
+            this.fileNameLabel.AutoSize = true;
+            this.fileNameLabel.Location = new System.Drawing.Point(453, 62);
+            this.fileNameLabel.Name = "fileNameLabel";
+            this.fileNameLabel.Size = new System.Drawing.Size(46, 13);
+            this.fileNameLabel.TabIndex = 15;
+            this.fileNameLabel.Text = "(No File)";
+            // 
+            // saveFileLabel
+            // 
+            this.saveFileLabel.AutoSize = true;
+            this.saveFileLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveFileLabel.Location = new System.Drawing.Point(9, 726);
+            this.saveFileLabel.Name = "saveFileLabel";
+            this.saveFileLabel.Size = new System.Drawing.Size(130, 13);
+            this.saveFileLabel.TabIndex = 16;
+            this.saveFileLabel.Text = "Save manipulated picture:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 749);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Save URL:";
+            // 
+            // saveFilePathTextBox
+            // 
+            this.saveFilePathTextBox.Location = new System.Drawing.Point(79, 746);
+            this.saveFilePathTextBox.Name = "saveFilePathTextBox";
+            this.saveFilePathTextBox.Size = new System.Drawing.Size(287, 20);
+            this.saveFilePathTextBox.TabIndex = 18;
+            // 
+            // sdfOpenButton
+            // 
+            this.sdfOpenButton.Location = new System.Drawing.Point(372, 744);
+            this.sdfOpenButton.Name = "sdfOpenButton";
+            this.sdfOpenButton.Size = new System.Drawing.Size(75, 23);
+            this.sdfOpenButton.TabIndex = 19;
+            this.sdfOpenButton.Text = "Choose...";
+            this.sdfOpenButton.UseVisualStyleBackColor = true;
+            this.sdfOpenButton.Click += new System.EventHandler(this.sdfOpenButton_Click);
+            // 
             // StegGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(586, 794);
+            this.ClientSize = new System.Drawing.Size(586, 822);
+            this.Controls.Add(this.sdfOpenButton);
+            this.Controls.Add(this.saveFilePathTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.saveFileLabel);
+            this.Controls.Add(this.fileNameLabel);
+            this.Controls.Add(this.ofdOpenButton);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.methodLabel);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.picturePathTextBox);
+            this.Controls.Add(this.initialPicturePathLabel);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.addKeyButton);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.hiddenInformationTextBox);
             this.Controls.Add(this.encryptRadioBtn);
             this.Controls.Add(this.decryptRadioBtn);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.initialPictureBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "StegGUI";
             this.Text = "Stegger";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.initialPictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox initialPictureBox;
         private System.Windows.Forms.RadioButton decryptRadioBtn;
         private System.Windows.Forms.RadioButton encryptRadioBtn;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox hiddenInformationTextBox;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label initialPicturePathLabel;
+        private System.Windows.Forms.TextBox picturePathTextBox;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button addKeyButton;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label methodLabel;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button ofdOpenButton;
+        private System.Windows.Forms.Label fileNameLabel;
+        private System.Windows.Forms.Label saveFileLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox saveFilePathTextBox;
+        private System.Windows.Forms.Button sdfOpenButton;
     }
 }
 
--- StegPic/StegPic/Forms/Form1.cs
+++ StegPic/StegPic/Forms/Form1.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +14,13 @@ namespace StegPic
 {
     public partial class StegGUI : Form
     {
+        Bitmap bmp = null;
+
         public StegGUI()
         {
             InitializeComponent();
         }
-
+        
         private void Form1_Load(object sender, EventArgs e)
         {
         }
@@ -25,13 +29,58 @@ namespace StegPic
         {
             encryptRadioBtn.Checked = decryptRadioBtn.Checked;
             decryptRadioBtn.Checked = !(decryptRadioBtn.Checked);
-
+            saveFileLabel.Text = "Save extracted information in .txt-file:";
         }
 
         private void encryptRadioBtn_Click(object sender, EventArgs e)
         {
             decryptRadioBtn.Checked = encryptRadioBtn.Checked;
             encryptRadioBtn.Checked = !(encryptRadioBtn.Checked);
+            saveFileLabel.Text = "Save manipulated picture:";
+        }
+
+        private void ofdOpenButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "PNG Image |*.png|JPEG Image |*.jpeg";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                picturePathTextBox.Text = ofd.FileName;
+                fileNameLabel.Text = ofd.SafeFileName;
+                initialPictureBox.ImageLocation = ofd.FileName;
+            }
+        }
+
+        private void sdfOpenButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (decryptRadioBtn.Checked)
+            {
+                sfd.Filter = "Text-file |*.txt";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    BinaryWriter bw = new BinaryWriter(File.Create(sfd.FileName));
+                    bw.Write(hiddenInformationTextBox.Text);
+                    bw.Dispose();
+                }
+            }
+            else if (encryptRadioBtn.Checked)
+            {
+                sfd.Filter = "PNG Image |*.png|JPEG Image |*.jpeg";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    //saving bitmap in diffrent imageformats based on FilterIndex of savefiledialog
+                    switch (sfd.FilterIndex)
+                    {
+                        case 0:
+                            bmp.Save(sfd.FileName, ImageFormat.Png);
+                            break;
+                        case 1:
+                            bmp.Save(sfd.FileName, ImageFormat.Jpeg);
+                            break;
+                    }
+                }                          
+            }
         }
     }
 }
